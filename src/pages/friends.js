ff344781e4e925ec9a923fd52993c14649562961
Kevin Kandlbinder <kevin@kevink.dev>
2020-12-22 10:37:25 +0100

    Add Friends Module

---
+++ src/pages/friends.js
@@ -0,0 +1,67 @@
+import React from "react"
+import Layout from "../layouts/default";
+import { Trans, Link, useI18next } from "gatsby-plugin-react-i18next"
+import { graphql } from 'gatsby'
+
+import styles from "./friends.module.scss";
+
+export const query = graphql`
+query AllFriendsQuery {
+    allFriendsJson {
+      nodes {
+        name
+        profession
+        url
+        imageURL
+      }
+    }
+  }
+  
+`
+
+export default function SocialPage({data}) {
+    
+    const {t} = useI18next();
+
+    function shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    return (
+        <Layout module="social" title={t("friends")} description={t("friendsDescription")}>
+            <section>
+                <article>
+                    <h1><Trans>social</Trans></h1>
+
+                    <p><Trans>friendsDescription</Trans></p>
+
+                    <div className={styles.friendsList}>
+                        {
+                            shuffle(data.allFriendsJson.nodes).map((friend) => {
+                                return (
+                                    <div className={styles.friendProfile}>
+                                        <div className={styles.friendImage} style={{backgroundImage: "url("+friend.imageURL+")"}}>
+                                            <span className={styles.friendName}>{friend.name}</span>
+                                            <span className={styles.friendTitle}>{friend.profession}</span>
+                                        </div>
+                                        
+                                        {/*<span class="friendBio"></span>*/}
+                                        <div className={styles.contactLinks}>
+                                            <a className={styles.contactLink} href={friend.url} target="_blank" rel="noopener"><i class="fas fa-globe-europe" aria-hidden="true"></i> {friend.url}</a>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+
+                    {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
+                </article>
+            </section>
+        </Layout>
+    );
+}
